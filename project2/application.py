600f505346b9bd5c35b8b0174bdff1c68d041d8a
Robert Senseman <robert.senseman@oculus.com>
2018-12-02 15:01:25 -0800

    ready for the big time

--- project2/application.py
+++ project2/application.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 from flask import Flask, request, session, render_template, redirect, jsonify
 from flask_socketio import SocketIO, emit
@@ -46,7 +47,7 @@ def load_home(channel_select=None):
 @app.route('/add_new_channel', methods=['POST'])
 def add_new_channel():
     query = text('''INSERT INTO channels ("channel_name", "creator") VALUES (:channel_name, :creator);''')
-    result = db.execute(query, {'channel_name': request.form.get('channel_name'), 'creator': session['username']})
+    db.execute(query, {'channel_name': request.form.get('channel_name'), 'creator': session['username']})
     db.commit()
     print(db.execute('select * from channels').fetchall())
     return 'channel_added'
@@ -63,7 +64,36 @@ def get_channel_messages(channel_name):
     session['last_page'] = channel_name
     if request.method == 'POST':
         print(f'getting messages for {channel_name}')
-        query = text("SELECT to_char(time_added, 'HH12:MI:SS AM'), username, message FROM messages WHERE channel=:channel_name ORDER BY time_added LIMIT 100")
+        # query = text("SELECT to_char(time_added, 'HH12:MI:SS AM'), username, message FROM messages WHERE channel=:channel_name ORDER BY time_added LIMIT 100")
+        query = text('''
+            SELECT
+                to_char(time_added, 'HH12:MI:SS AM') time_added_formatted
+                ,username
+                ,message
+                ,uid
+                ,COALESCE(smile_count, 0) smile_count
+                ,COALESCE(frown_count, 0) frown_count
+                ,COALESCE(sad_count, 0) sad_count
+                ,COALESCE(shrug_count, 0) shrug_count
+            FROM (
+                SELECT
+                    *
+                FROM messages
+                WHERE channel=:channel_name
+            ) messages
+            LEFT JOIN (
+                SELECT
+                    message_id
+                    ,SUM(CASE reaction WHEN 'smile' THEN 1 ELSE 0 END) smile_count
+                    ,SUM(CASE reaction WHEN 'frown' THEN 1 ELSE 0 END) frown_count
+                    ,SUM(CASE reaction WHEN 'sad' THEN 1 ELSE 0 END) sad_count
+                    ,SUM(CASE reaction WHEN 'shrug' THEN 1 ELSE 0 END) shrug_count
+                FROM reactions
+                GROUP BY 1
+            ) reactions ON messages.uid=reactions.message_id
+            ORDER BY time_added
+            LIMIT 100
+        ''')
         rows = db.execute(query, {'channel_name': channel_name}).fetchall()
         print([row.values() for row in rows])
         return jsonify({'messages': [row.values() for row in rows]})
@@ -73,19 +103,32 @@ def get_channel_messages(channel_name):
 
 @app.route('/submit_message', methods=['POST'])
 def submit_message():
-    query = text('''INSERT INTO messages ("time_added", "channel", "username", "message") VALUES (to_timestamp(:timestamp), :channel, :username, :message);''')
+    query = text('''INSERT INTO messages ("time_added", "channel", "username", "message", "uid") VALUES (to_timestamp(:timestamp), :channel, :username, :message, :uid);''')
+    uid = str(uuid.uuid4())
     result = db.execute(query, {
                                 'timestamp': request.form.get('timestamp'),
-                                'channel': request.form.get('channel_name'),
+                                'channel': request.form.get('channel_name').split('?')[0],
                                 'username': session['username'],
                                 'message': request.form.get('message_text'),
+                                'uid': uid
                                 }
     )
     print(db.execute('select * from channels').fetchall())
     print(result)
     db.commit()
-    return 'message_stored'
+    return jsonify({'message_uid': uid})
 
+@app.route('/submit_vote', methods=['POST'])
+def submit_vote():
+    query = text('''INSERT INTO reactions ("message_id", "reaction", "time_added") VALUES (:message_id, :reaction, to_timestamp(:timestamp));''')
+    result = db.execute(query, {
+                                'message_id': request.form.get('message_id'),
+                                'reaction': request.form.get('vote'),
+                                'timestamp': request.form.get('timestamp')
+                                }
+    )
+    db.commit()
+    return 'reaction added'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -112,3 +155,21 @@ def login():
 def broadcast_new_message(data):
     emit('new_message', data, broadcast=True)
     return 'message emitted'
+
+@socketio.on('new_vote')
+def broadcast_new_vote(data):
+    query = text('''INSERT INTO reactions ("message_id", "reaction", "time_added") VALUES (:message_id, :reaction, to_timestamp(:timestamp));''')
+    print(data)
+    db.execute(query, {
+                       # 'message_id': request.form.get('message_id'),
+                       # 'reaction': request.form.get('vote'),
+                       # 'timestamp': request.form.get('timestamp')
+                       'message_id': data['message_id'],
+                       'reaction': data['vote'],
+                       'timestamp': data['timestamp']
+                       }
+               )
+    db.commit()
+
+    emit('new_vote', data, broadcast=True)
+    return 'reaction recorded'
--- project2/static/chatroom.js
+++ project2/static/chatroom.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     channels = document.getElementsByClassName('channel-in-channel-list')
     for (channel of channels) {
-        channel.href='javascript:'
+        channel.href='javascript:';
         channel.onclick=show_channel;
     }
 
@@ -40,8 +40,28 @@ document.addEventListener('DOMContentLoaded', function() {
         channel_submitted_to = data.channel_name;
         url_arr = window.location.href.substring(0).split('/');
         if ((url_arr.length > 4) && (url_arr[url_arr.length-1]===channel_submitted_to)) {
-            add_new_message(data.date_string, data.username, data.message_text);
+            add_new_message(data.date_string, data.username, data.message_text, data.message_id);
         }
+        return false;
+    });
+
+    socket.on('new_vote', data => {
+        console.log('new vote here')
+        console.log(data)
+
+        message_container = document.getElementById(data.message_id);
+        console.log(message_container);
+        if (message_container) {
+            emoji_block = message_container.children[2];
+            console.log(emoji_block)
+            emoji_block.childNodes.forEach((node) => {
+            console.log(node.children[0].getAttribute('class'));
+                if (node.children[0].getAttribute('class') == `emoji-${data.vote}`) {
+            	       node.children[1].innerHTML = (parseInt(node.children[1].innerHTML) + 1).toString();
+          	    }
+            });
+        }
+
         return false;
     });
 
@@ -75,14 +95,12 @@ function finalize_channel() {
     const channel_name = this.firstElementChild.value;
 
     request.open('POST', '/add_new_channel');
-    request.onload = () => {
-        const request_data = JSON.parse(request.responseText);
-    }
     const data = new FormData();
     data.append('channel_name', channel_name);
     request.send(data);
 
     link = document.createElement("A");
+    link.href='javascript:';
     link.onclick = show_channel;
     link.setAttribute("class", "channel-in-channel-list")
     // link.setAttribute("href", `/channel/${channel_name}`)
@@ -108,17 +126,26 @@ function show_channel(channel_name) {
     if (!(typeof channel_name === 'string' || channel_name instanceof String)) {
         channel_name = this.innerHTML;
     }
+    console.log(channel_name);
+    selectors = document.getElementsByClassName('channel-in-channel-list')
+    for (selector of selectors) {
+        if (selector.text === channel_name) {
+            selector.style['fontWeight']='bold';
+        } else {
+            selector.style['fontWeight']='normal';
+        }
+    }
 
     const request = new XMLHttpRequest();
     request.open('POST', `/channel/${channel_name}`);
     request.onload = () => {
         const request_data = JSON.parse(request.responseText);
-        const messages = Object.entries(request_data.messages)
+        const messages = Object.entries(request_data.messages);
         for (message_arr of messages) {
-            console.log(message_arr)
-            console.log(message_arr[1])
-            let [time, user, message] = message_arr[1]
-            add_new_message(time, user, message)
+            console.log(message_arr);
+            console.log(message_arr[1]);
+            let [time, user, message, message_id, smile_count, frown_count, sad_count, shrug_count] = message_arr[1];
+            add_new_message(time, user, message, message_id, [smile_count, frown_count, sad_count, shrug_count]);
         }
 
         if (document.getElementById("channel-content").children.length === 0) {
@@ -139,10 +166,13 @@ function add_intro() {
     );
 }
 
-function add_new_message(date_string, username, message) {
+function add_new_message(date_string, username, message_text, message_id, emoji_votes) {
     container = document.getElementById("channel-content");
-    new_text = document.createElement("div");
+    group_container = document.createElement("div");
+    group_container.setAttribute("id", `${message_id}`);
 
+    // message text
+    new_text = document.createElement("div");
     time_string = document.createElement("span");
     time_string.setAttribute("class", "time-sent");
     time_string.appendChild(document.createTextNode(`${date_string} `));
@@ -153,12 +183,57 @@ function add_new_message(date_string, username, message) {
 
     new_content = document.createElement("span");
     new_content.setAttribute("class", "message-text");
-    new_content.appendChild(document.createTextNode(`${message}`));
+    new_content.appendChild(document.createTextNode(`${message_text}`));
+
+    // message emoji votes
+    emoji_block = document.createElement("div");
+
+    emoji_votes = emoji_votes || [0,0,0,0];
+    emoji_names = ['smile', 'frown', 'sad', 'shrug'];
+    emoji_array = ['&#x1F600', '&#x1F641', '&#x1F622', '&#x1F937'];
+    for (i = 0; i < emoji_array.length; i++) {
+        button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "emoji-button");
+        button.onclick = function() {
+            date = new Date();
+            date_timestamp = date.getTime();
+
+            vote = this.firstChild.getAttribute('class').split('-')[1]
+
+            console.log({
+                'message_id': message_id,
+                'vote': vote,
+                'timestamp': date_timestamp
+            });
+            socket.emit('new_vote', {
+                'message_id': message_id,
+                'vote': vote,
+                'timestamp': date_timestamp
+            });
+        }
+
+        emoji = document.createElement('span');
+        emoji.setAttribute('class', `emoji-${emoji_names[i]}`);
+        emoji.innerHTML = emoji_array[i];
+
+        votes = document.createElement('span');
+        votes.setAttribute('class', 'vote-count');
+        votes.innerHTML = emoji_votes[i];
+
+        button.appendChild(emoji);
+        button.appendChild(votes);
+        emoji_block.appendChild(button);
+    }
 
     new_text.appendChild(time_string);
     new_text.appendChild(preface);
     new_text.appendChild(new_content);
-    container.appendChild(new_text);
+    group_container.appendChild(new_text);
+    group_container.appendChild(document.createElement('br'));
+    group_container.appendChild(emoji_block);
+    group_container.appendChild(document.createElement('br'));
+    container.appendChild(group_container);
 
     return false;
 }
@@ -181,20 +256,25 @@ function submit_message() {
     const request = new XMLHttpRequest();
     request.open('POST', '/submit_message');
 
+    request.onload = () => {
+        const request_data = JSON.parse(request.responseText);
+        uid = request_data.message_uid;
+
+        // emit the submission
+        socket.emit('new_message', {
+            'channel_name': channel_name,
+            'message_text': message_text,
+            'date_string': date_string,
+            'username': username,
+            'message_id': uid
+        });
+    }
     const data = new FormData();
     data.append('channel_name', channel_name);
     data.append('message_text', message_text);
     data.append('timestamp', date_timestamp);
 
     request.send(data);
-
-    // emit the submission
-    socket.emit('new_message', {
-        'channel_name': channel_name,
-        'message_text': message_text,
-        'date_string': date_string,
-        'username': username
-    });
 
     // reset field and local storage to empty strings
     localStorage.setItem('user_input', '')
